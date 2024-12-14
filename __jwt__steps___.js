d6611bc491354bc0202c055412086a506655c6de
Programming Hero <programming.hero1@gmail.com>
2024-12-14 11:55:35 -0600

    generate token and set to cookie

--- __jwt__steps___.js
+++ __jwt__steps___.js
@@ -4,4 +4,13 @@
  * ------------------------
  * 
  * install jsonwebtoken cookie-parser
+ * set cookieParser as middleware
+ * 
+ * 1. create a token
+ * 
+ * jwt.sign(data, secret, {expiresIn: '5h'})
+ * 
+ * set token to the cookie of res
+ * 
+ * 
 */
